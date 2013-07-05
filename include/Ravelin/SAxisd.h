1be26d0f8f6062329c3ec74f291f05ac074edb11
Evan Drumwright <drum@gwu.edu>
2013-07-04 23:45:41 -0500

    Added spatial axis

---
+++ include/Ravelin/SAxisd.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+ * Copyright 2013 Evan Drumwright
+ * This library is distributed under the terms of the GNU Lesser General Public 
+ * License (found in COPYING).
+ ****************************************************************************/
+
+#ifndef _SAXISD_H
+#define _SAXISD_H
+
+namespace Ravelin {
+
+#include "ddefs.h"
+#include "SAxis.h"
+#include "undefs.h"
+
+} // end namespace
+
+#endif
+
---
+++ include/Ravelin/SAxisf.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+ * Copyright 2013 Evan Drumwright
+ * This library is distributed under the terms of the GNU Lesser General Public 
+ * License (found in COPYING).
+ ****************************************************************************/
+
+#ifndef _SAXISF_H
+#define _SAXISF_H
+
+namespace Ravelin {
+
+#include "fdefs.h"
+#include "SAxis.h"
+#include "undefs.h"
+
+} // end namespace
+
+#endif
+
